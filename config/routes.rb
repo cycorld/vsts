ccfbd90371ed6e7d5370f2ad46e63c6f4a2461db
cycorld <charleschoi87@gmail.com>
2016-04-16 15:12:27 +0900

    layout materialize css

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'home/rank'
 
   resources :ranks
+
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
